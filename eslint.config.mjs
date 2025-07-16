98da183269c3255e068501a2f73472c8c496f374
Abhi198511Maurya <abhishekkumarmaurya1545@gmail.com>
2025-07-16 09:27:24 +0530

    Add MyAppointments page and Build the maximum frontend

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,21 +6,27 @@ import { defineConfig } from "eslint/config";
 export default defineConfig([
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    ignores: [
-      "node_modules/*",
-      ".husky/*", // optional: ignore config files
-    ],
-    plugins: { js },
-    extends: ["js/recommended"],
+    ignores: ["node_modules/**", ".husky/**"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true, // 👈 enable JSX
+        },
+      },
+    },
+    plugins: {
+      react: pluginReact,
+    },
     rules: {
       "react/react-in-jsx-scope": "off",
-      "no-unused-vars": "off",
       "react/prop-types": "off",
+      "no-unused-vars": "off",
     },
   },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: globals.browser },
-  },
-  pluginReact.configs.flat.recommended,
 ]);
