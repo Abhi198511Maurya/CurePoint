5054ba29e10d11564b99b5ec38337a1ec73e8f36
Abhi198511Maurya <abhishekkumarmaurya1545@gmail.com>
2025-09-27 13:08:55 +0530

    Fix SMTP, send mail feature

--- backend/config/nodemailer.js
+++ backend/config/nodemailer.js
@@ -4,12 +4,10 @@ const transporter = nodemailer.createTransport({
   host: "smtp-relay.brevo.com",
   port: 587,
   secure: false,
-  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASSWORD,
   },
-  connectionTimeout: 30000,
 });
 
 export default transporter;
